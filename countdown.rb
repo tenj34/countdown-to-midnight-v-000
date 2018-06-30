fa08afa447edd2855bdef81a55f558b5a3db8af4
Juan Tenezaca <tenj34@gmail.com>
2018-06-30 16:59:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,10 +3,10 @@
 def countdown(number)
   counter = number
 
-  while counter !< 0
+  while counter <! 0
     puts " #{counter} SECOND(S)!"
     --counter
   end
-  
+
   return "HAPPY NEW YEAR"
 end
